fdfae2ec016244cb66f524a69b4e7bbca5e46100
danilkushnerov <apple.pacel@gmail.com>
2024-06-24 18:10:48 +0300

    подключил postgre

--- PolyWebdemos/settings.py
+++ PolyWebdemos/settings.py
@@ -54,7 +54,8 @@ ROOT_URLCONF = 'PolyWebdemos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates']
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,11 +76,14 @@ WSGI_APPLICATION = 'PolyWebdemos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'webdemos_bd',
+        'USER': 'postgres',
+        'PASSWORD': '1401',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
